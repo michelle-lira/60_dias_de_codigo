d163dd29613612848df3ef5be7a95c51c4999fbf
Michelle Lira <mchll.lira@gmail.com>
2021-09-02 16:39:13 -0300

    Atualiza repositorio

--- dia_07/metros_para_centimetros.py
+++
@@ -1,5 +0,0 @@
-# Faça um programa que converta metros para centímetros.
-metros = int(input('Digite o valor em metros: '))
-centimetros = (metros * 100)
-
-print(f'{metros} metros é igual a {centimetros} centímetros')
--- dia_07/raio_do_circulo.py
+++
@@ -1,7 +0,0 @@
-# Faça um programa que peça o raio de um círculo, calcule e mostre sua área.
-import math
-
-raio = int(input('Digite o valor da medida do raio de um círculo: '))
-area = (2 * math.pi * raio)
-
-print(f'Um círculo com raio {raio} possui área igual a {area}.')
